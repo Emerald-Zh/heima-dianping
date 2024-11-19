5970284a9255a5252fae7b11bbbcd355d21ed417
Emerald-Zh <emeraldqi29@gmail.com>
2024-11-19 20:36:20 +0800

    一人一单

---
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -0,0 +1,174 @@
+package com.hmdp.controller;
+
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.entity.UserInfo;
+import com.hmdp.service.IUserInfoService;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.MailUtils;
+import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
+
+import static com.hmdp.utils.RedisConstants.*;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private IUserInfoService userInfoService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 发送手机验证码
+     */
+    @PostMapping("/code")
+    public Result sendCode(@RequestParam("phone") String phone, HttpSession session) throws MessagingException {
+        // TODO 发送短信验证码并保存验证码
+        if (RegexUtils.isEmailInvalid(phone)) {
+            return Result.fail("邮箱格式不正确");
+        }
+        String code = MailUtils.achieveCode();
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
+        log.info("发送登录验证码：{}", code);
+//        MailUtils.sendTestMail(phone, code);
+        return Result.ok();
+    }
+
+    /**
+     * 登录功能
+     *
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session) {
+        // TODO 实现登录功能
+        //获取登录账号
+        String phone = loginForm.getPhone();
+        //获取登录验证码
+        String code = loginForm.getCode();
+        //获取session中的验证码
+        //Object cacheCode = session.getAttribute(phone);
+
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
+
+        //1. 校验邮箱
+        if (RegexUtils.isEmailInvalid(phone)) {
+            //2. 不符合格式则报错
+            return Result.fail("邮箱格式不正确！！");
+        }
+        //3. 校验验证码
+        log.info("code:{},cacheCode{}", code, cacheCode);
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            // 不一致，报错
+            return Result.fail("验证码错误");
+        }
+        //5. 根据账号查询用户是否存在
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getPhone, phone);
+        User user = userService.getOne(queryWrapper);
+        //6. 如果不存在则创建
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+        //7. 保存用户信息到session中
+        //7. 保存用户信息到Redis中
+        //7.1 随机生成token，作为登录令牌
+        String token = UUID.randomUUID().toString();
+        //7.2 将UserDto对象转为HashMap存储
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        HashMap<String, String > userMap = new HashMap<>();
+        userMap.put("icon", userDTO.getIcon());
+        userMap.put("id", String.valueOf(userDTO.getId()));
+        userMap.put("nickName", userDTO.getNickName());
+//        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
+//                CopyOptions.create()
+//                        .setIgnoreNullValue(true)
+//                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+        String tokenKey = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+        stringRedisTemplate.expire(tokenKey, 30, TimeUnit.MINUTES);
+        stringRedisTemplate.delete(LOGIN_CODE_KEY + phone);
+        session.setAttribute("user", userDTO);
+        return Result.ok(token);
+    }
+
+    private User createUserWithPhone(String phone) {
+        //创建用户
+        User user = new User();
+        //设置手机号
+        user.setPhone(phone);
+        //设置昵称(默认名)，一个固定前缀+随机字符串
+        user.setNickName("user_" + RandomUtil.randomString(8));
+        //保存到数据库
+        userService.save(user);
+        return user;
+    }
+
+    /**
+     * 登出功能
+     *
+     * @return 无
+     */
+    @PostMapping("/logout")
+    public Result logout() {
+        // TODO 实现登出功能
+        return Result.fail("功能未完成");
+    }
+
+    @GetMapping("/me")
+    public Result me() {
+        // TODO 获取当前登录的用户并返回
+        UserDTO user = UserHolder.getUser();
+        return Result.ok(user);
+    }
+
+    @GetMapping("/info/{id}")
+    public Result info(@PathVariable("id") Long userId) {
+        // 查询详情
+        UserInfo info = userInfoService.getById(userId);
+        if (info == null) {
+            // 没有详情，应该是第一次查看详情
+            return Result.ok();
+        }
+        info.setCreateTime(null);
+        info.setUpdateTime(null);
+        // 返回
+        return Result.ok(info);
+    }
+}
---
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -0,0 +1,192 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.RedisData;
+import com.hmdp.entity.Shop;
+import com.hmdp.mapper.ShopMapper;
+import com.hmdp.service.IShopService;
+import com.hmdp.utils.CacheClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+@Slf4j
+public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private CacheClient cacheClient;
+
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    @Override
+    public Result queryById(Long id) {
+        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, 20L, TimeUnit.SECONDS);
+//        Shop shop = queryWithLogicalExpire(id);
+        if (shop == null) {
+            return Result.fail("店铺不存在！！");
+        }
+        return Result.ok(shop);
+    }
+
+    @Override
+    public Shop queryWithPassThrough(Long id) {
+        //先从Redis中查，这里的常量值是固定的前缀 + 店铺id
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        //如果不为空（查询到了），则转为Shop类型直接返回
+        if (StrUtil.isNotBlank(shopJson)) {
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        if (shopJson != null) {
+            return null;
+        }
+        //否则去数据库中查
+        Shop shop = getById(id);
+        //查不到，则将空值写入Redis
+        if (shop == null) {
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
+        }
+        //查到了则转为json字符串
+        String jsonStr = JSONUtil.toJsonStr(shop);
+        //并存入redis，设置TTL
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, jsonStr, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        //最终把查询到的商户信息返回给前端
+        return shop;
+    }
+
+    @Override
+    public Shop queryWithMutex(Long id) {
+        //先从Redis中查，这里的常量值是固定的前缀 + 店铺id
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        //如果不为空（查询到了），则转为Shop类型直接返回
+        if (StrUtil.isNotBlank(shopJson)) {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        if (shopJson != null) {
+            return null;
+        }
+        Shop shop = null;
+        try {
+            //否则去数据库中查
+            boolean flag = tryLock(LOCK_SHOP_KEY + id);
+            if (!flag) {
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+            //查不到，则将空值写入Redis
+            shop = getById(id);
+            if (shop == null) {
+                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+            //查到了则转为json字符串
+            String jsonStr = JSONUtil.toJsonStr(shop);
+            //并存入redis，设置TTL
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, jsonStr, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+            //最终把查询到的商户信息返回给前端
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            unlock(LOCK_SHOP_KEY + id);
+        }
+        return shop;
+    }
+
+
+    @Override
+    public Shop queryWithLogicalExpire(Long id) {
+        //1. 从redis中查询商铺缓存
+        String json = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        //2. 如果未命中，则返回空
+        if (StrUtil.isBlank(json)) {
+            return null;
+        }
+        //3. 命中，将json反序列化为对象
+        RedisData redisData = JSONUtil.toBean(json, RedisData.class);
+        JSONObject shopJson = (JSONObject) redisData.getData();
+        Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        //4. 判断是否过期
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            //5. 过期，直接返回商铺信息
+            log.info("过期，直接返回店铺信息");
+            return shop;
+        }
+        //6. 过期，尝试获取互斥锁
+        boolean flag = tryLock(LOCK_SHOP_KEY + id);
+        //7. 获取到了锁
+        if (flag) {
+            //8. 开启独立线程
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    this.saveShop2Redis(id, LOCK_SHOP_TTL);
+                    log.info("独立线程完成缓存重构");
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unlock(LOCK_SHOP_KEY + id);
+                }
+            });
+            //9. 直接返回商铺信息
+            return shop;
+        }
+        //10. 未获取到锁，直接返回商铺信息
+        return shop;
+    }
+
+    @Override
+    public Result update(Shop shop) {
+        //首先先判一下空
+        if (shop.getId() == null) {
+            return Result.fail("店铺id不能为空！！");
+        }
+        //先修改数据库
+        updateById(shop);
+        //再删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
+        return Result.ok();
+    }
+
+    private boolean tryLock(String key) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+    public void saveShop2Redis(Long id, Long expirSeconds) {
+        Shop shop = getById(id);
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expirSeconds));
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(redisData));
+    }
+}
